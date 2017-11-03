629424a801baf181f96416997870de02e9c8ae88
mibo <mibo@mirb.de>
2017-11-03 20:48:22 +0100

    Initial handshake

--- src/main/kotlin/de/mirb/pg/socks/SocketForwardHandler.kt
+++ src/main/kotlin/de/mirb/pg/socks/SocketForwardHandler.kt
@@ -5,11 +5,17 @@ import org.slf4j.LoggerFactory
 import java.net.Socket
 import java.nio.ByteBuffer
 import java.nio.channels.Channels
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.util.*
 import java.util.concurrent.TimeUnit
+import javax.xml.bind.DatatypeConverter
+import javax.xml.crypto.dsig.DigestMethod
 
 class SocketForwardHandler(
     private val host: String,
     private val port: Int,
+    private val webSocketSupportEnabled: Boolean = true,
     private val timeoutSeconds: Long = 10) : SocketHandler {
 
   private val LOG = LoggerFactory.getLogger(this.javaClass.name)
@@ -50,14 +56,24 @@ class SocketForwardHandler(
         inBuffer.flip()
         val stream = ContentHelper.toStream(inBuffer)
         LOG.trace("Received: '{}'; start forward to {}", stream.asString(), client.connection())
-        val response = client.send(inBuffer)
-        LOG.trace("Successful forwarded: '{}' (to {})", stream.asString(), client.connection())
-        val responseStream = ContentHelper.toStream(response)
-        LOG.trace("Got response (from: {}): {}", client.connection(), responseStream.asString())
-        LOG.trace("Write response (from {}) back to forward connection ({})", localConnection, client.connection())
-        outChannel.write(response)
-        inBuffer.clear()
-        LOG.trace("Successful wrote response back for connection {}", client.connection())
+        if(webSocketSupportEnabled && isWebSocketRequest(stream)) {
+          LOG.trace("Received web socket upgrade request '{}'", localConnection)
+          val wsResponse = createWebSocketResponse(stream)
+          outChannel.write(wsResponse)
+          inBuffer.clear()
+          LOG.trace("Successful wrote ws upgrade response back for connection {} with response content \n'{}'",
+              localConnection, ContentHelper.toStream(wsResponse).asString())
+        } else {
+          val response = client.send(inBuffer)
+          LOG.trace("Successful forwarded: '{}' (to {})", stream.asString(), client.connection())
+          val responseStream = ContentHelper.toStream(response)
+          LOG.trace("Got response (from: {}): {}", client.connection(), responseStream.asString())
+          LOG.trace("Write response (from {}) back to forward connection ({})", localConnection, client.connection())
+          outChannel.write(response)
+          inBuffer.clear()
+          LOG.trace("Successful wrote response back for connection {}", client.connection())
+        }
+
       }
     }
 
@@ -67,6 +83,42 @@ class SocketForwardHandler(
     socket.close()
     client.close()
   }
+
+  private fun isWebSocketRequest(stream: ContentHelper.Stream): Boolean {
+    if(stream.asString().startsWith("GET")) {
+      return true
+    }
+    return false
+  }
+
+  private val SEC_WS_KEY_HEADER = "Sec-WebSocket-Key"
+
+  private fun createWebSocketResponse(stream: ContentHelper.Stream): ByteBuffer {
+    val lines = stream.asString().lines()
+    val secWebSocketKey = lines.singleOrNull { l -> l.startsWith(SEC_WS_KEY_HEADER) }
+    // handle failure
+    if(secWebSocketKey == null) {
+      return ByteBuffer.wrap("HTTP/1.1 400 Bad Request".toByteArray())
+    }
+    val value = secWebSocketKey.substring(SEC_WS_KEY_HEADER.length+1).trim() + WS_GUID
+    val md = MessageDigest.getInstance("SHA1")
+    val digest = md.digest(value.toByteArray(StandardCharsets.ISO_8859_1))
+    val secWsAcceptValue = String(Base64.getEncoder().encode(digest))
+    val res = WS_RESPONSE_TEMPLATE.replace("<accept>", secWsAcceptValue)
+    val tmp = res.toByteArray(StandardCharsets.US_ASCII)
+    return ByteBuffer.wrap(tmp)
+  }
+
+  // https://tools.ietf.org/html/rfc6455.html#section-1.3
+  private val WS_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+  private val EOL = "\r\n"
+  private val WS_RESPONSE_TEMPLATE =
+      "HTTP/1.1 101 Switching Protocols" + EOL +
+      "Server: mibo pg websocket" + EOL +
+      "Connection: Upgrade" + EOL +
+      "Upgrade: websocket" + EOL +
+      "Sec-WebSocket-Version: 13" + EOL +
+      "Sec-WebSocket-Accept: <accept>" + EOL + EOL
 
   override fun stop() {
     run = false
